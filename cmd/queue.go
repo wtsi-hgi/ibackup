28b70bb27b5a1d3e8f1fd08bc9bc4bd948fdb798
Sendu Bala <sb10@sanger.ac.uk>
2023-01-23 14:22:07 +0000

    Add stub for new queue sub-command.

---
+++ cmd/queue.go
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Genome Research Ltd.
+ *
+ * Authors:
+ *	- Sendu Bala <sb10@sanger.ac.uk>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// options for this cmd.
+var queueAll bool
+var queueUser string
+var queueSet string
+var queuePath string
+var queueKick bool
+var queueDelete bool
+
+// queueCmd represents the queue command.
+var queueCmd = &cobra.Command{
+	Use:   "queue",
+	Short: "Administer the server queue",
+	Long: `Administer the server queue.
+
+The user who started the server can use this sub-command to retry or remove
+buried items in the server's global put queue.
+
+You need to supply the ibackup server's URL in the form domain:port (using the
+IBACKUP_SERVER_URL environment variable, or overriding that with the --url
+argument) and if necessary, the certificate (using the IBACKUP_SERVER_CERT
+environment variable, or overriding that with the --cert argument).
+
+You can --retry or --delete --all currently buried items, or specify which ones
+by setting --user and --name (apply to all buried items in the set with that
+name belonging to that user). You can also add a local --path to limit to the a
+single file in that set.
+
+Specifying nothing displays details about all currently buried items in the
+queue.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ensureURLandCert()
+
+		_, err := getPasswordFromServerTokenFile()
+		if err != nil {
+			die("Only the user who started the server can use this sub-command.")
+		}
+
+		if queueDelete && queueKick {
+			die("-d and -r are mutually exclusive")
+		}
+
+		if queueDelete || queueKick {
+			if queuePath != "" && (queueUser == "" || queueSet == "") {
+				die("--path needs --user and --name")
+			}
+
+			if queueSet != "" && queueUser == "" {
+				die("--name needs --user")
+			}
+
+			if queueAll && queueUser != "" {
+				die("--all is mutually exclusive from --user, --name and --path")
+			}
+
+			if queueUser == "" && !queueAll {
+				die("at least one of --all or --user must be provided")
+			}
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(queueCmd)
+
+	// flags specific to this sub-command
+	queueCmd.Flags().StringVarP(&queueUser, "user", "u", "", "limit -d/-r to sets of this user")
+	queueCmd.Flags().StringVarP(&queueSet, "name", "n", "", "limit -d/-r to this backup set")
+	queueCmd.Flags().StringVarP(&queuePath, "path", "p", "", "limit -d/-r to this local file in the set")
+	queueCmd.Flags().BoolVarP(&queueAll, "all", "a", false,
+		"apply -d/-r to all buried items in the queue")
+	queueCmd.Flags().BoolVarP(&queueDelete, "delete", "d", false,
+		"delete certain buried items in the queue")
+	queueCmd.Flags().BoolVarP(&queueKick, "retry", "r", false,
+		"retry certain buried items in the queue")
+}
