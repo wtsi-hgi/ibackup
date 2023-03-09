66ef754be578b5a9394379a4cae5e54137115414
Sendu Bala <sb10@sanger.ac.uk>
2023-03-09 10:00:06 +0000

    Only replace error if there was an output message.

--- put/put.go
+++ put/put.go
@@ -128,7 +128,7 @@ type FileReadTester func(ctx context.Context, path string) error
 func headRead(ctx context.Context, path string) error {
 	out, err := exec.CommandContext(ctx, "head", "-c", "1", path).CombinedOutput()
 
-	if err != nil {
+	if err != nil && len(out) > 0 {
 		err = Error{msg: string(out)}
 	}
 
