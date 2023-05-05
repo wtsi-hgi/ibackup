96ab66c17d88c12ac80ceec712d7735ffbe2cd3e
Michael Woolnough <mw31@sanger.ac.uk>
2023-05-05 11:10:13 +0100

    restructured entry creation and updating code

--- set/inode.go
+++ set/inode.go
@@ -1,84 +1,45 @@
 package set
 
 import (
-	"os"
+	"sort"
 	"strconv"
 	"strings"
-	"syscall"
 
+	"github.com/moby/sys/mountinfo"
 	"github.com/ugorji/go/codec"
 	"github.com/wtsi-ssg/wrstat/v4/walk"
 	bolt "go.etcd.io/bbolt"
 )
 
-// statAndUpdatePureFileEntry checks if the given path exists. If it exists and
-// is a link the entry is updated to reflect if it's a hard or symlink.
-func (d *DB) statAndUpdatePureFileEntry(tx *bolt.Tx, entry *Entry) error {
-	info, err := os.Lstat(entry.Path)
+// getMountPoints retrieves a list of mount point paths to be used when
+// determining hardlinks. The list is sorted longest first and stored on the
+// server object.
+func (d *DB) getMountPoints() error {
+	mounts, err := mountinfo.GetMounts(func(info *mountinfo.Info) (bool, bool) {
+		switch info.FSType {
+		case "devpts", "devtmpfs", "cgroup", "rpc_pipefs", "fusectl",
+			"binfmt_misc", "sysfs", "debugfs", "tracefs", "proc", "securityfs",
+			"pstore", "mqueue", "hugetlbfs", "configfs":
+			return true, false
+		}
+
+		return false, false
+	})
 	if err != nil {
 		return err
 	}
 
-	statt, ok := info.Sys().(*syscall.Stat_t)
-	if !ok {
-		return nil
+	d.mountList = make([]string, len(mounts))
+
+	for n, mp := range mounts {
+		d.mountList[n] = mp.Mountpoint
 	}
 
-	de := &walk.Dirent{
-		Path:  entry.Path,
-		Type:  info.Mode().Type(),
-		Inode: statt.Ino,
-	}
+	sort.Slice(d.mountList, func(i, j int) bool {
+		return len(d.mountList[i]) > len(d.mountList[j])
+	})
 
-	entry.Inode = statt.Ino
-
-	return d.updateEntryWithTypeDetails(tx, de, entry)
-}
-
-func (d *DB) updateEntryWithTypeDetails(tx *bolt.Tx, de *walk.Dirent, entry *Entry) error {
-	if de.IsDir() {
-		entry.Type = Directory
-
-		return nil
-	}
-
-	eType, err := d.direntToEntryType(tx, de)
-	if err != nil {
-		return err
-	}
-
-	entry.Type = eType
-	entry.Dest = ""
-
-	if eType == Symlink {
-		entry.Dest, err = os.Readlink(de.Path)
-	}
-
-	return err
-}
-
-// direntToEntryType returns missing, symlink or hardlink status if the
-// Dirent is one of those (default Pending).
-func (d *DB) direntToEntryType(tx *bolt.Tx, de *walk.Dirent) (EntryType, error) {
-	eType := Regular
-
-	switch {
-	case de.Inode == 0:
-		eType = Unknown
-	case de.IsSymlink():
-		eType = Symlink
-	default:
-		isHardLink, err := d.handleInode(tx, de)
-		if err != nil {
-			return eType, err
-		}
-
-		if isHardLink {
-			eType = Hardlink
-		}
-	}
-
-	return eType, nil
+	return nil
 }
 
 // handleInode recordes the inode of the given Dirent in the database, and
