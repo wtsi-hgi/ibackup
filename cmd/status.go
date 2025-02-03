7c0af4e01bc5b49c82155bee0c5b0a5e76a37cd6
Rosie Kern <rk18@sanger.ac.uk>
2025-02-03 15:33:07 +0000

    Combine removeFiles and removeDirs endpoints and add recently removed to status

--- cmd/status.go
+++ cmd/status.go
@@ -410,8 +410,8 @@ func displaySet(s *set.Set, showRequesters bool) { //nolint:funlen,gocyclo
 	}
 
 	cliPrintf("Discovery: %s\n", s.Discovered())
-	cliPrintf("Num files: %s; Symlinks: %d; Hardlinks: %d; Size (total/recently uploaded): %s / %s\n",
-		s.Count(), s.Symlinks, s.Hardlinks, s.Size(), s.UploadedSize())
+	cliPrintf("Num files: %s; Symlinks: %d; Hardlinks: %d; Size (total/recently uploaded/recently removed): %s / %s / %s\n",
+		s.Count(), s.Symlinks, s.Hardlinks, s.Size(), s.UploadedSize(), s.RemovedSize())
 	cliPrintf("Uploaded: %d; Replaced: %d; Skipped: %d; Failed: %d; Missing: %d; Abnormal: %d\n",
 		s.Uploaded, s.Replaced, s.Skipped, s.Failed, s.Missing, s.Abnormal)
 
